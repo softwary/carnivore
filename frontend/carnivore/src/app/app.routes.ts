1d0b164e657ac8822b4ee2f4b5a1afa32443fa35
Dominique Adevai <dominiqueadevai@gmail.com>
2024-03-04 20:50:52 -0500

    Fix URL path issue.

--- frontend/carnivore/src/app/app.routes.ts
+++ frontend/carnivore/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { GameBoardComponent } from './game-board/game-board.component';
 
 export const routes: Routes = [
-  { path: 'game-board', component: GameBoardComponent },
+  { path: 'game-board-d', component: GameBoardComponent },
 ];
+
